23ec8a28dd67ba31e8136ce8f49b479a327caa18
putri lannidya <putri.lannidya@gmail.com>
2020-12-24 14:11:29 +0700

    ui: add splash screen

---
+++ app/src/main/java/com/lanin/liveattendanceapp/views/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.lanin.liveattendanceapp.views.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lanin.liveattendanceapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
