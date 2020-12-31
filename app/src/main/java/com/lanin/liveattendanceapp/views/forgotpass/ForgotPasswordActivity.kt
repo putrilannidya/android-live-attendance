4ca0f8be712626128f159e050ab51df45e31d06c
putri lannidya <putri.lannidya@gmail.com>
2020-12-31 12:21:20 +0700

    feat: Prepare networking for consume API

--- app/src/main/java/com/lanin/liveattendanceapp/views/forgotpass/ForgotPasswordActivity.kt
+++ app/src/main/java/com/lanin/liveattendanceapp/views/forgotpass/ForgotPasswordActivity.kt
@@ -15,6 +15,14 @@ class ForgotPasswordActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         init()
+
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.tbForgotPassword.setNavigationOnClickListener {
+            finish()
+        }
     }
 
     private fun init() {
--- app/src/main/java/com/lanin/liveattendanceapp/views/login/LoginActivity.kt
+++ app/src/main/java/com/lanin/liveattendanceapp/views/login/LoginActivity.kt
@@ -3,10 +3,30 @@ package com.lanin.liveattendanceapp.views.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lanin.liveattendanceapp.R
+import org.jetbrains.anko.startActivity
+import com.lanin.liveattendanceapp.databinding.ActivityLoginBinding
+import com.lanin.liveattendanceapp.views.forgotpass.ForgotPasswordActivity
+import com.lanin.liveattendanceapp.views.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.btnLogin.setOnClickListener {
+            startActivity<MainActivity>()
+        }
+
+        binding.btnForgotPassword.setOnClickListener {
+            startActivity<ForgotPasswordActivity>()
+        }
     }
 }
